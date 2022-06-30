6cb10f86a0060c4342c12c75497a9b95d2a112c7
lukas <lukasvalentin.graf@usys.ethz.ch>
2022-06-30 11:28:27 +0200

    updating reference to rtm_config arg

--- scripts/invert_s2_scenes.py
+++ scripts/invert_s2_scenes.py
@@ -23,99 +23,11 @@ from eodal.operational.mapping import MapperConfigs
 from pathlib import Path
 from typing import List, Optional, Union
 
+from rtm_inv.core.config import RTMConfig, LookupTableBasedInversion
 from rtm_inv.core.inversion import inv_img, retrieve_traits
 from rtm_inv.core.lookup_table import generate_lut
 
 logger = get_settings().logger
-
-class RTMConfig:
-    """
-    Radiative transfer model inversion configuration set-up
-    """
-    def __init__(
-            self,
-            traits: List[str],
-            rtm_params: Path,
-            rtm: Optional[str] = 'prosail'
-        ):
-        """
-        Class Constructor
-
-        :param traits:
-            list of traits to retrieve from the inversion process
-            (depending on radiative transfer model chosen)
-        :param rtm_params:
-            CSV-file specifying RTM specific parameter ranges and their
-            distribution
-        :param rtm:
-            name of the radiative transfer model to use. 'prosail' by
-            default
-        """
-        self.traits = traits
-        self.rtm_params = rtm_params
-        self.rtm = rtm
-
-        @property
-        def rtm_params() -> Path:
-            return self.rtm_params
-
-        @property
-        def traits() -> List[str]:
-            return self.traits
-
-        @property
-        def rtm() -> str:
-            return self.rtm
-
-class LookupTableBasedInversion(RTMConfig):
-    """
-    Lookup-table based radiative transfer model inversion set-up
-    """
-    def __init__(
-            self,
-            n_solutions: int,
-            cost_function: str,
-            lut_size: int,
-            method: Optional[str] = 'LHS',
-            **kwargs
-        ):
-        """
-        Class constructor
-
-        :param n_solutions:
-            number of solutions to use for lookup-table based inversion
-        :param cost_function:
-            name of the cost-function to evaluate similarity between
-            observed and simulated spectra
-        :param lut_size:
-            size of the lookup-table (number of spectra to simulate)
-        :param method:
-            method to use for sampling the lookup-table
-        :param kwargs:
-            keyword arguments to pass to super-class
-        """
-        # call constructor of super-class
-        super().__init__(**kwargs)
-        self.n_solutions = n_solutions
-        self.cost_function = cost_function
-        self.method = method
-        self.lut_size = lut_size
-
-        @property
-        def lut_size() -> int:
-            return self.lut_size
-
-        @property
-        def n_solutions() -> int:
-            return self.n_solutions
-
-        @property
-        def cost_function() -> str:
-            return self.cost_function
-
-        @property
-        def method() -> str:
-            return self.method
 
 def traits_from_s2(
         date_start: date,
@@ -144,7 +56,7 @@ def traits_from_s2(
     :param date_end:
         end date for performing the trait retrieval
     :param aoi:
-        area of interest for which to peform the trait retrieval
+        area of interest for which to perform the trait retrieval
     :param rtm_config:
         radiative transfer model configuration (forward and inverse)
     :param output_dir:
@@ -257,13 +169,13 @@ def traits_from_s2(
                 lut=s2_lut_spectra,
                 img=s2_spectra,
                 mask=mask,
-                cost_function=cost_function,
-                n_solutions=n_solutions,
+                cost_function=rtm_config.cost_function,
+                n_solutions=rtm_config.n_solutions,
             )
             trait_img = retrieve_traits(
                 lut=lut,
                 lut_idxs=lut_idxs,
-                traits=traits
+                traits=rtm_config.traits
             )
 
             # save traits to file
