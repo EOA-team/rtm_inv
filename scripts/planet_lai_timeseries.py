61d815e81fd55439aca6bd09c2cc52fe35cf5da1
lukas <lukasvalentin.graf@usys.ethz.ch>
2022-07-01 12:30:14 +0200

    minor changes in attribute names

--- scripts/planet_lai_timeseries.py
+++ scripts/planet_lai_timeseries.py
@@ -10,10 +10,10 @@ import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
 
-fpath = Path('/mnt/ides/Lukas/04_Work/PS_Eschikon_TS/timeseries_BW_lai.gpkg')
+fpath = Path('/mnt/ides/Lukas/04_Work/PS_Eschikon_TS/timeseries_BW_medians_lai.gpkg')
 gdf = gpd.read_file(fpath)
 
-out_dir = fpath.parent.joinpath('figures')
+out_dir = fpath.parent.joinpath('figures_medians')
 out_dir.mkdir(exist_ok=True)
 
 plt.style.use('ggplot')
@@ -26,20 +26,23 @@ ax.set_ylabel('Frequency [-]')
 f.savefig(out_dir.joinpath('all_ps_lai_histogram.png'), bbox_inches='tight')
 
 # visualize LAI time series for the sampling points
-points = gdf.Point_ID.unique()
+points = gdf.Point_ID_caller.unique()
 
 for point in points:
-    point_pixels = gdf[gdf.Point_ID == point].copy()
-    point_pixels.acquired = pd.to_datetime(point_pixels.acquired)
-    point_pixels['date'] = point_pixels.acquired.apply(lambda x: x.date())
-    point_pixels = point_pixels.sort_values(by='acquired')
-    f, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 7))
-    ax.scatter(point_pixels.acquired, point_pixels.lai)
-    ax.set_ylabel(r'PlanetScope Green Leaf Area Index [$m^2$/$m^2$]')
-    ax.set_xlabel('Time')
-    ax.set_title(f'Bramenwies - Point {point}')
-    ax.set_ylim(0,7)
-    plt.xticks(rotation=45)
-    fname = out_dir.joinpath(f'{point}_ps_lai_timeseries.png')
-    f.savefig(fname, bbox_inches='tight')
-    plt.close(f)
+    try:
+        point_pixels = gdf[gdf.Point_ID_caller == point].copy()
+        point_pixels.acquired_other = pd.to_datetime(point_pixels.acquired_other)
+        point_pixels['date'] = point_pixels.acquired_other.apply(lambda x: x.date())
+        point_pixels = point_pixels.sort_values(by='acquired_other')
+        f, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 7))
+        ax.scatter(point_pixels.acquired_other, point_pixels.lai)
+        ax.set_ylabel(r'PlanetScope Green Leaf Area Index [$m^2$/$m^2$]')
+        ax.set_xlabel('Time')
+        ax.set_title(f'Bramenwies - Point {point}')
+        ax.set_ylim(0,7)
+        plt.xticks(rotation=45)
+        fname = out_dir.joinpath(f'{point}_ps_lai_timeseries.png')
+        f.savefig(fname, bbox_inches='tight')
+        plt.close(f)
+    except Exception as e:
+        print(e)
